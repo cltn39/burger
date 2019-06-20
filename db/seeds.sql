12e3fcbaa36552ed61be4fdbf9f7090741df2066
gravitation <chisong@ucdavis.edu>
2019-06-19 22:47:27 -0700

    database created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devour) VALUES (famous burger, false);
+INSERT INTO burgers (burger_name, devour) VALUES (guac burger, false);
+INSERT INTO burgers (burger_name, devour) VALUES (double cheese burger, false);
