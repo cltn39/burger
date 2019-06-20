e909b1aa80b3132ea781a1156ba3be334ceb5cab
gravitation <chisong@ucdavis.edu>
2019-06-19 23:48:45 -0700

    added models

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// Import the ORM
+const orm = require("../config/orm");
+
+const burger = {
+    all: (cb) => {
+        orm.all("burgers", (res) => {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    },
+    delete: (condition, cb) => {
+        orm.delete("burgers", condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
