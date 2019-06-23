75b650cdb2d57ff21047f6888a8a98535ee25caf
gravitation <chisong@ucdavis.edu>
2019-06-22 20:28:46 -0700

    fixed db format

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,19 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
-use burgers_db
+use burgers_db;
 
-CREATE TABLE burgers 
-(
+CREATE TABLE burgers (
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(60) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE beverages (
+ id INTEGER NOT NULL AUTO_INCREMENT,
+    beverage_name VARCHAR(60) NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    burger_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devour) VALUES (famous burger, false);
-INSERT INTO burgers (burger_name, devour) VALUES (guac burger, false);
-INSERT INTO burgers (burger_name, devour) VALUES (double cheese burger, false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Aburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cburger", true);
