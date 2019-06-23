8ad0298a4e6f7348b18c37e3be2453fc3118aacb
gravitation <chisong@ucdavis.edu>
2019-06-22 20:25:36 -0700

    added UI

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
-
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,7 +16,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/catsController.js");
+const routes = require("./controllers/burgers_controller");
 
 app.use(routes);
 
